9ecd24baea6cecf1067adc3e48baa63f23674e1b
Jacob M <48130732+Andacanaver@users.noreply.github.com>
2022-03-23 05:58:31 -0500

    Update ScrollButton.js
    Updated scroll button from event listener only to use useEffect.

--- src/components/ScrollButton/ScrollButton.js
+++ src/components/ScrollButton/ScrollButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowCircleUp } from '@fortawesome/free-solid-svg-icons'
 import { Button } from './Styles'
@@ -7,6 +7,18 @@ import { Button } from './Styles'
 export const ScrollButton = () => {
   const [visible, setVisible] = useState(false)
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setVisible(true)
+      } else {
+        setVisible(false)
+      }
+    });
+  }, [])
+
+  /*
+  --Old version of button
   const toggleVisible = () => {
     const scrolled = document.documentElement.scrollTop;
     if (scrolled > 300) {
@@ -15,7 +27,7 @@ export const ScrollButton = () => {
       setVisible(false)
     }
   }
-
+*/
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -23,12 +35,12 @@ export const ScrollButton = () => {
     })
   }
 
-  window.addEventListener('scroll', toggleVisible)
+  //window.addEventListener('scroll', toggleVisible) --old method
 
   return(
     <Button>
-      <FontAwesomeIcon icon={faArrowCircleUp} onClick={scrollToTop}
-      style={{display: visible ? 'inline' : 'none'}} />
+      {visible &&
+      <FontAwesomeIcon icon={faArrowCircleUp} onClick={scrollToTop}/> }
     </Button>
   )
 }
